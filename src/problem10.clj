f798ad0944fa6acaad96db8f8ccd74795c3dcab9
Nishant Rayan <nishantrayan>
2011-03-15 12:49:04 +0530

    solution to problem 10.. takes 15 seconds to throw out answer... can it be optimized?

--- src/problem10.clj
+++ src/problem10.clj
@@ -1,14 +1,25 @@
 (ns problem10)
+(def limit 2000000)
+(def primes (ref [2]))
 (defn is-prime?
-    "checks if a number is prime and returns true if its prime"
+    "checks if a number is prime by iterating over all primes from 2 to sqrt(n) and looking for potential factors"
     [n]
-    (let [limit (Math/ceil (Math/sqrt n))]
-        (if (= n 2) true
-            (if(= n 1) false
-                        (loop [current 2]
-                            (if (> current limit)
-                                true
-                                (if (zero? (rem n current))
-                                    false
-                                        (recur (inc current)))))))))
-(println (apply + (filter is-prime? (range 1 2000000))))
+    (let [limit (int (Math/ceil (Math/sqrt n))) primes-vector @primes count-primes (count primes-vector)]
+        (loop [i 0]
+            (if (>= i count-primes)
+                true
+                (let [p (nth primes-vector i)]
+                    (if (> p limit)
+                        true
+                        (if (zero? (mod n p))
+                            false
+                            (recur (inc i)))))))))
+(println (apply + 2
+                (for [n (range 3 2000000)]
+                        (if (is-prime? n)
+                            (do
+                                (dosync
+                                    (alter primes #(conj % n)))
+                                n)
+                            0))))
+
