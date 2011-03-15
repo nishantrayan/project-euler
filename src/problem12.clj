c3d6f7dc944188b6347041d63dd4ceb517fba194
Nishant Rayan <nishant@nishant-laptop.(none)>
2011-03-15 19:02:16 +0530

    solved problem 12. elegant!

---
+++ src/problem12.clj
@@ -0,0 +1,25 @@
+(ns problem12)
+(defn count-factors
+    "returns the number of unique factors that the number has"
+    [n]
+    (let [limit (int (Math/floor (Math/sqrt n)))]
+        (loop [current 1 count-divisors 0]
+            (if (> current limit)
+                count-divisors
+                (recur (inc current)
+                        (+ count-divisors
+                            (if (zero? (mod n current))
+                                (if (= current (Math/sqrt n))
+                                    1 2)
+                                0)))))))
+(assert (= (count-factors 5) 2))
+(assert (= (count-factors 6) 4))
+(assert (= (count-factors 9) 3))
+(assert (= (count-factors 16) 5))
+
+(println (loop [current 1 sum 0]
+    (if (> (count-factors sum) 500)
+        sum
+        (recur (inc current)
+                (+ sum current)))))
+
