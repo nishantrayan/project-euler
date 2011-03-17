fb2ae5f1ff9d36eca335ea7db67972d9875e8832
Nishant Rayan <nishant@nishant-laptop.(none)>
2011-03-17 21:41:24 +0530

    solution to problem 14. using memoization.

--- src/problem14.clj
+++ src/problem14.clj
@@ -3,17 +3,17 @@
 (defn find-depth
     "finds depth of the number by applying a function : n/2 [n is event] : 3n+1 [n is odd]"
     [n]
-    (if (= n 1)
-        1
-        (let [depth-from-map (depth-map n)]
-            (if (not (nil? depth-from-map))
-                depth-from-map
-                (let [next-n (if (even? n) (int (/ n 2)) (+ (* 3 n) 1))
-                    depth-from-recur (inc (find-depth next-n))]
-                    (dosync (alter depth-map #(assoc % n depth-from-recur)))
-                    depth-from-recur)))))
-(assert (= (find-depth 2) 2))
-(assert (= 10 (find-depth 13)))
-(println (find-depth 1000000))
-(println (apply max
-    (map #(find-depth %) (range 1 1000001))))
+    (loop [current-n n depth-so-far 1]
+            (if (= current-n 1)
+                (do
+                    (dosync (alter depth-map #(assoc % current-n depth-so-far)))
+                    depth-so-far)
+                (let [depth-in-map (depth-map current-n)]
+                    (if (not (nil? depth-in-map))
+                        depth-in-map
+                        (let [next-n (if (even? current-n) (bit-shift-right current-n 1) (+ (* 3 current-n) 1))]
+                            (recur next-n (inc depth-so-far))))))))
+(println (inc
+            (let [depth-list (map #(find-depth %) (range 1 1000001))]
+                (.indexOf depth-list (reduce max depth-list)))))
+
