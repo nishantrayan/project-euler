edf0f21d342ef6990222080feb014b8d3171979c
Nishant Rayan <nishant@nishant-laptop.(none)>
2011-03-17 22:49:18 +0530

    without tail recursion and proper memoization at every step. runs much faster (20s)

--- src/problem14.clj
+++ src/problem14.clj
@@ -3,17 +3,15 @@
 (defn find-depth
     "finds depth of the number by applying a function : n/2 [n is event] : 3n+1 [n is odd]"
     [n]
-    (loop [current-n n depth-so-far 1]
-            (if (= current-n 1)
-                (do
-                    (dosync (alter depth-map #(assoc % current-n depth-so-far)))
-                    depth-so-far)
-                (let [depth-in-map (depth-map current-n)]
-                    (if (not (nil? depth-in-map))
-                        depth-in-map
-                        (let [next-n (if (even? current-n) (bit-shift-right current-n 1) (+ (* 3 current-n) 1))]
-                            (recur next-n (inc depth-so-far))))))))
-(println (inc
-            (let [depth-list (map #(find-depth %) (range 1 1000001))]
-                (.indexOf depth-list (reduce max depth-list)))))
+    (if (= n 1)
+        1
+        (let [depth-from-map (depth-map n)]
+            (if (not (nil? depth-from-map))
+                depth-from-map
+                (let [next-n (if (even? n) (bit-shift-right n 1) (+ (* 3 n) 1))
+                    depth-from-recur (inc (find-depth next-n))]
+                    (dosync (alter depth-map #(assoc % n depth-from-recur)))
+                    depth-from-recur)))))
+(println (inc (let [depth-list (map #(find-depth %) (range 1 1000001))]
+    (.indexOf depth-list (reduce max depth-list)))))
 
